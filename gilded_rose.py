934410108ad8161092566972705ac51ca54175c8
sunyirong <sun.yiro@northeastern.edu>
2025-02-12 21:18:50 -0800

    refactor: refactor the items and fix the tests

--- gilded_rose.py
+++ gilded_rose.py
@@ -3,13 +3,60 @@
 
 class Item:
     """ DO NOT CHANGE THIS CLASS!!!"""
-    def __init__(self, name, sell_in, quality):
+    def __init__(self, name, sell_in, quality, conjured=False):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
+        self.degrade_rate = 1
+        if conjured:
+            self.degrade_rate = 2
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class NormalItem(Item):
+    def __init__(self, sell_in, quality, conjured=False):
+        super().__init__("Item", sell_in, quality, conjured)
+
+    def update_quality(self):
+        if self.quality > 0:
+            self.quality -= self.degrade_rate
+        self.sell_in -= 1
+        if self.sell_in < 0 and self.quality > 0:
+            self.quality -= self.degrade_rate
+
+class AgedBrie(Item):
+    def __init__(self, sell_in, quality, conjured=False):
+        super().__init__("Aged Brie", sell_in, quality, conjured)
+
+    def update_quality(self):
+        if self.quality < 50:
+            self.quality += 1
+        self.sell_in -= 1
+        if self.sell_in < 0 and self.quality < 50:
+            self.quality += 1
+
+class Sulfuras(Item):
+    def __init__(self, sell_in, quality, conjured=False):
+        super().__init__("Sulfuras", sell_in, quality, conjured)
+
+    def update_quality(self):
+        pass
+
+class BackstagePass(Item):
+    def __init__(self, sell_in, quality, conjured=False):
+        super().__init__("Backstage pass", sell_in, quality, conjured)
+        
+    def update_quality(self):
+        if self.quality < 50:
+            self.quality += 1
+            if self.sell_in < 11 and self.quality < 50:
+                self.quality += 1
+            if self.sell_in < 6 and self.quality < 50:
+                self.quality += 1
+        self.sell_in -= 1
+        if self.sell_in < 0:
+            self.quality = 0
 
 
 class GildedRose(object):
@@ -20,30 +67,15 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            item.update_quality()
+    
+    def sell(self, itemType: type, amount: int):
+        for item in self.items:
+            if isinstance(item, itemType):
+                self.items.remove(item)
+                amount -= 1
+            if amount == 0:
+                break
+    
+    def get_items(self)->list[str]:
+        return [item.name for item in self.items]
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,57 +1,57 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import *
 
 
 class GildedRoseTest(unittest.TestCase):
     # example of test that checks for logical errors
     def test_sulfuras_should_not_decrease_quality(self):
-        items = [Item("Sulfuras", 5, 80)]
+        items = [Sulfuras(5, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         sulfuras_item = items[0]
-        self.assertEquals(80, sulfuras_item.quality)
-        self.assertEquals(4, sulfuras_item.sell_in)
-        self.assertEquals("Sulfuras", sulfuras_item.name)
+        self.assertEqual(80, sulfuras_item.quality)
+        self.assertEqual(5, sulfuras_item.sell_in)
+        self.assertEqual("Sulfuras", sulfuras_item.name)
 
     # example of test that checks for syntax errors
     def test_gilded_rose_list_all_items(self):
-        items = [Item("Sulfuras", 5, 80)]
+        items = [Sulfuras(5, 80)]
         gilded_rose = GildedRose(items)
         all_items = gilded_rose.get_items()
-        self.assertEquals(["Sulfuras"], all_items)
+        self.assertEqual(["Sulfuras"], all_items)
 
     # tests whether normal items's quality degrades correcly after sellin day
     def test_twice_quality_degrades(self):
-        items = [Item("Normal", 1, 10)]
+        items = [NormalItem(1, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality() # quality 10->9
         gilded_rose.update_quality() # quality 9->7
         normal_item = items[0]
-        self.assertEquals(normal_item.quality, 8)
+        self.assertEqual(normal_item.quality, 7)
 
     # tests whether backstage pass's quality is correctly updated before sellin
     def test_backstage_pass_quality_before_sellin(self):
-        items = [Item("backstage passes", 1, 3)]
+        items = [BackstagePass(1, 3)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(items[0].quality, 6)
+        self.assertEqual(items[0].quality, 6)
 
     # tests whether backstage pass's quality is correctly updated after sellin
     def test_backstage_pass_quality_after_sellin(self):
-        items = [Item("backstage passes", 1, 5)]
+        items = [BackstagePass(1, 5)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         gilded_rose.update_quality()
-        self.assertEquals(items[0].quality, 0)
+        self.assertEqual(items[0].quality, 0)
 
     # tests giled_rose sell function
     def test_gilded_rose_sell_item(self):
-        items = [Item("A", 1, 5)]
+        items = [NormalItem("A", 1, 5)]
         gilded_rose = GildedRose(items)
-        gilded_rose.sell("A", 1)
-        self.assertEquals(len(items), 0)
+        gilded_rose.sell(NormalItem, 1)
+        self.assertEqual(len(items), 0)
 
 
 if __name__ == '__main__':
